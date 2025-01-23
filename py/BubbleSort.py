8364327ebbaf515e179ed41ab22080ef6964f266
Vimala-Suram <vimalasuram5@gmail.com>
2025-01-23 13:25:50 -0500

    Implememnted bubble sort

--- py/BubbleSort.py
+++ py/BubbleSort.py
@@ -18,5 +18,12 @@ def bubble_sort(arr):
         """
         arr[i], arr[j] = arr[j], arr[i]
 
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                swap(j, j + 1)
+    return arr
+
     # TODO: Implement the bubble sort algorithm below.
     pass
