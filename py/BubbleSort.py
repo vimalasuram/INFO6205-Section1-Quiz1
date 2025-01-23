9e9b0afc30ab70c9b2a82cf06408553f5cb52d0d
saxena-akshit <saxena.aksh@northeastern.edu>
2025-01-23 10:22:21 -0500

    feat: add helper method

--- py/BubbleSort.py
+++ py/BubbleSort.py
@@ -8,4 +8,15 @@ def bubble_sort(arr):
     Returns:
         list: The sorted list.
     """
+    def swap(i, j):
+        """
+        Swap two elements in the list at indices i and j.
+
+        Args:
+            i (int): Index of the first element.
+            j (int): Index of the second element.
+        """
+        arr[i], arr[j] = arr[j], arr[i]
+
+    # TODO: Implement the bubble sort algorithm below.
     pass
